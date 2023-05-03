dd4c95efa47e64ca1a95eb5aea94ecd8823f55d4
milosRank <mrankovic310@gmail.com>
2023-05-03 19:48:40 +0200

    ystral mixing page done

---
+++ ystral-mixing.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Meta -->
+    <?php include 'partials/resources/meta.php';?>
+    
+    <!-- CSS -->
+    <?php include 'partials/resources/styles.php';?>
+
+    <!-- FAVICON -->
+    <?php include 'partials/resources/favicon.php';?>
+
+    <title>YSTRAL Mixing | RM Alcon Stahl</title>
+
+</head>
+
+<body>
+
+    <!-- Page wrapper start -->
+    <div class="page-warpper">
+
+        <!-- Header start -->
+        <?php
+            $header_bg_color = "bg-white"; // Set header background color
+            include 'partials/elements/header.php';
+        ?> <!-- Header end -->
+
+
+        <!-- Main start -->
+        <main>
+
+            <!-- Page header start -->
+            <section class="page-header page-header-bg--ystral-mixing container-narrow">
+                <div class="container-fluid">
+                    <div class="wrapper">
+                        <div class="container">
+
+                            <!-- Page header inner start -->
+                            <div class="page-header__inner">
+
+                                <div class="title">
+                                    <h1>YSTRAL - Mixing solutions</h1>
+                                </div>
+
+                            </div> <!-- Page header inner end -->
+
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Page header end -->
+
+            <!-- Text and image start -->
+            <section class="text-and-image text-and-image--reversed text-wider container-narrow">
+                <div class="container-fluid">
+                    <div class="wrapper">
+                        <div class="container">
+
+                            <!-- Text and image inner start -->
+                            <div class="text-and-image__inner">
+
+                                <!-- Text start -->
+                                <div class="text">
+                                    <div class="text__inner">
+                                        <div class="part-content">
+                                            <p><strong>RM Alkon Stahl</strong> je zastupnik firme <a href="#">YSTRAL GmbH</a> iz Nemačke. </p>
+                                            <p>Ystral je već dugo vremena dobro poznato ime u procesnoj tehnologiji. </p>
+                                            <p>Ystral je proizvođač miksera, dispergatora, homogenizatora kao i kombinovanih mešača - dispergatora.</p>
+                                            <p>
+                                                Njihovi uređaji efikasno mogu da zamene mašine kao što je klasičan disolver (dissolver) i postižu znatno 
+                                                bolje rezultate od njih u realizaciji operacija indukcije, kvašenja i mešanja praškastih sirovina koje se 
+                                                uvode i homogenizuju sa tečnim komponentama odnosno rastvaračima.
+                                            </p>
+                                            <p>
+                                                U ponudi su uređaji veličine od najmanjih laboratorijskih do velikih industrijskih mašina, kao 
+                                                i kompletna rešenja za određene proizvodne procese.Korisnicima svoje opreme Ystral nudi svesrdnu 
+                                                podršku na bazi dugogodišnjeg iskustva u oblasti tehnologije proizvodnje i prerade najrazličitijih praškastih sirovina.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div> <!-- Text end -->
+
+                                <!-- Image start -->
+                                <div class="image">
+                                    <div class="image__inner border">
+                                        <img width="337" height="229" src="assets/img/ystral/ystral.jpg" alt="Ystral mixing">
+                                    </div>
+                                </div> <!-- Image end -->
+
+                            </div> <!-- Text and image inner end -->
+
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Text and image end -->
+
+        </main> <!-- Main end -->
+
+
+        <!-- Footer -->
+        <?php include 'partials/elements/footer.php';?>
+
+        <!-- Postfooter -->
+        <?php include 'partials/elements/postfooter.php';?>
+
+    </div> <!-- Page wrapper end -->
+
+    <!-- Scripts -->
+    <?php include 'partials/resources/scripts.php';?>
+</body>
+
+</html>
