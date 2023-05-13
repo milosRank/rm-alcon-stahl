b12a035e336cdc0c8700b3c106afb12105ead311
milosRank <mrankovic310@gmail.com>
2023-05-13 18:06:47 +0200

    Preuzmi page done

---
+++ preuzmi.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- Meta -->
+    <?php include 'partials/resources/meta.php';?>
+    
+    <!-- CSS -->
+    <?php include 'partials/resources/styles.php';?>
+
+    <!-- FAVICON -->
+    <?php include 'partials/resources/favicon.php';?>
+
+    <title>Preuzmi | RM Alcon Stahl</title>
+
+</head>
+
+<body>
+
+    <!-- Page wrapper start -->
+    <div class="page-warpper">
+
+        <!-- Header start -->
+        <?php
+            $header_bg_color = "bg-white"; // Set header background color
+            include 'partials/elements/header.php';
+        ?> <!-- Header end -->
+
+
+        <!-- Main start -->
+        <main>
+
+            <!-- Page header start -->
+            <section class="page-header page-header-bg--preuzmi">
+                <div class="container-fluid">
+                    <div class="wrapper">
+                        <div class="container">
+
+                            <!-- Page header inner start -->
+                            <div class="page-header__inner">
+
+                                <div class="title">
+                                    <h1>Preuzmi</h1>
+                                </div>
+
+                            </div> <!-- Page header inner end -->
+
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Page header end -->
+
+            <!-- Text and image start -->
+            <section class="text-and-image text-and-image--reversed">
+                <div class="container-fluid">
+                    <div class="wrapper">
+                        <div class="container">
+
+                            <!-- Text and image inner start -->
+                            <div class="text-and-image__inner">
+
+                                <!-- Text start -->
+                                <div class="text">
+                                    <div class="text__inner">
+                                        <div class="part-content">
+                                            <div class="title">
+                                                <h2>Nerđajući čelici <br>  <span class="font-primary-regular">(Inox materijali)</span></h2>
+                                            </div>
+                                            <ul class="pdf-list">
+                                                <li><a href="#" download>Nerđajući čelici - uporedna tabela kvaliteta <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Limovi - (kg/tabli) formati i završna obrada površina <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Plazma sečenje - slika <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Šipke - (kg/m) okrugle, kvadratne, šestougaone <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Šipke - tolerancije ISO h-j-k <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Šipke - (kg/m) flah i profili <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>U profili - dimenzije (kg/m) <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>I profili - dimenzije (kg/m) <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - (kg/m) kvadratne i pravougaone <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - (kg/m) okrugle <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - tolerancije prema EN 10217-7 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - hemijski sastav prema EN 10217 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - mehaničke osobine prema EN 10217 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Cevi - (kg/m) bešavne i debelozide <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Fitinzi - slika <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Prirubnice prema EN 1092 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Ventili i armatura - slike <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Ventili i armatura - dimenzije <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Zavarivanje - uporedne oznake i pakovanje <span class="text-color-blue">(PDF)</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div> <!-- Text end -->
+
+                                <!-- Image start -->
+                                <div class="image">
+                                    <div class="image__inner">
+                                        <img width="648" height="1037" src="assets/img/corporate/preuzmi-nerdjajuci-celici.jpg" alt="Nerdjajuci celici">
+                                    </div>
+                                </div> <!-- Image end -->
+
+                            </div> <!-- Text and image inner end -->
+
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Text and image end -->
+
+            <!-- Text and image start -->
+            <section class="text-and-image text-and-image--reversed">
+                <div class="container-fluid">
+                    <div class="wrapper">
+                        <div class="container">
+
+                            <!-- Text and image inner start -->
+                            <div class="text-and-image__inner">
+
+                                <!-- Text start -->
+                                <div class="text">
+                                    <div class="text__inner">
+                                        <div class="part-content">
+                                            <div class="title">
+                                                <h2>Inox ograde</h2>
+                                            </div>
+                                            <ul class="pdf-list">
+                                                <li><a href="#" download>Q-railing katalog 2021/2022 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Q-Glass - Bezbednosno staklo se može prepoznati po pečatu <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Easy Glass System brošura - 2011 <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Easy Glass System - Brzo se sastavlja i instalira, štedeći Vaše vreme i novac <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Square Line - Revolucionaran kvalitetni koncept kvadratnih profila <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Q-Smart - Pametan način instalacije sistema ograda <span class="text-color-blue">(PDF)</span></a></li>
+                                                <li><a href="#" download>Q-Railing ekskluzivni sistem staklenih ograda <span class="text-color-blue">(PDF)</span></a></li>
+                                                
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div> <!-- Text end -->
+
+                                <!-- Image start -->
+                                <div class="image">
+                                    <div class="image__inner">
+                                        <img width="648" height="411" src="assets/img/corporate/preuzmi-inox-ograde.jpg" alt="Inox ograde">
+                                    </div>
+                                </div> <!-- Image end -->
+
+                            </div> <!-- Text and image inner end -->
+
+                        </div>
+                    </div>
+                </div>
+            </section> <!-- Text and image end -->
+
+        </main> <!-- Main end -->
+
+
+        <!-- Footer -->
+        <?php include 'partials/elements/footer.php';?>
+
+        <!-- Postfooter -->
+        <?php include 'partials/elements/postfooter.php';?>
+
+    </div> <!-- Page wrapper end -->
+
+    <!-- Scripts -->
+    <?php include 'partials/resources/scripts.php';?>
+</body>
+
+</html>
